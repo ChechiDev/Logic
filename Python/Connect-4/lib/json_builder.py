b3e22e879912eb8dedaa91f30ec9f69d89430cb6
ChechiDev <chechidatadev@gmail.com>
2025-06-28 12:50:52 +0200

    ADD: json_builder.py => check_foder: Checks if the folder exists, creates it if not, and returns the folder path

--- Python/Connect-4/lib/json_builder.py
+++ Python/Connect-4/lib/json_builder.py
@@ -1,23 +1,35 @@
 import os
 import json
-from lib.exceptions import ExceptionBase
+# from lib.exceptions import ExceptionBase
 
 class JSONBuilder:
     def __init__(self, file_path: str):
         self._file_path = file_path
 
 
-def check_folder(self):
-    pass
+    def check_folder(self):
+        folder = os.path.dirname(self._file_path)
+
+        if folder and not os.path.exists(folder):
+            os.makedirs(folder)
+            print(f"Folder {folder} created")
+
+        else:
+            print(f"Folder {folder} already exists!")
+
+        return folder
 
 
-def build_json(self):
-    pass
+    def build_json(self):
+        pass
 
 
-def load_json(self):
-    pass
+    def load_json(self):
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    file_path = r"data\test.json"
+    builder = JSONBuilder(file_path)
+
+    builder.check_folder()
